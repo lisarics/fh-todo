b36ee2f020fd3687646764785fef784453c10cbd
lisarics <lisa.richardson@gmail.com>
2018-09-15 15:43:57 +0000

    set up toggling of the tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe TasksController, type: :controller do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
       get :index
-      expect(response).to have_http_status :success
+      expect(response).to have_http_status(:success)
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
     end
